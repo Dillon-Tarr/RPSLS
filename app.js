b04d0d6bbd063864bd8f2b6bb3052eb001926883
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-08-20 17:52:15 -0400

    figured out how to capture user input using a form and how to use it in JavaScript
    Prepared the next bit of action.

--- app.js
+++ app.js
@@ -3,34 +3,42 @@
 let interactiveContent = document.getElementById("interactiveWindow");
 
 function mainMenu(){
-  interactiveContent.innerHTML = `<form id="form" name="">
-  I want to play best of <label for="bestOf"><input id="bestOf" name="bestOf" type="number" class="numberInput" value="3" min="3" step="1" max="21"></label> against 
+  interactiveContent.innerHTML =
+  `<form id="form" name="form">
+  I want to play best of
+  <label for="bestOf"><input id="bestOf" name="bestOf" type="number" class="numberInput" value="3" min="3" step="2" max="23"></label> against 
   <label for="againstWhom"><select id="againstWhom" name="againstWhom" class="dropdownInput">
     <option value="computer">the computer</option>
     <option value="human">the person next to me</option>
   </select></label>.<br>
-  <div class="buttonHolder"><label for="start"><input id="start" type="submit" name="start" value="Start!"></label></div>
+  <div class="buttonHolder"><label for="start"><input id="startButton" type="submit" name="start" value="Start!"></label></div>
 </form>`;
+  let formData = document.getElementById("form");
+  form.onsubmit = function(e) {
+    e.preventDefault();
+    let bestOf = formData.bestOf.value;
+    let againstWhom = formData.againstWhom.value;
+    playGame(bestOf, againstWhom);
+  }
 } 
 
-function playGame(e) {
-  let interactiveWindow = document.querySelector("#interactiveWindow"),
-      form = interactiveWindow.querySelectorAll("#form"),
-      submitInput = form[0].querySelector('input[type="submit"]');
-
-  e.preventDefault();
-  let formData = new FormData(form);
-  alert(`Here's your form data:
-bestOf: ${formData.get("bestOf")}
-againstWhom: ${formData.get("againstWhom")}
-
-Good job, Dillon!!`)
-  
+function playGame(bestOf, againstWhom) {
+  if (againstWhom == "computer"){
+    playAgainstComputer(bestOf);
+  }
+  if (againstWhom == "human"){
+    playAgainstHuman(bestOf);
+  }
 }
 
+function playAgainstComputer(bestOf){
+  let pointsToWin = bestOf / 2 + 0.5;
+  console.log(`You will now play against the computer. First to ${pointsToWin} points wins!`)
+}
 
-
-
-
+function playAgainstHuman(bestOf){
+  let pointsToWin = bestOf / 2 + 0.5;
+  console.log(`You will now play against each other. First to ${pointsToWin} points wins!`)
+}
 
 
