60a7f6c4a4a3a2aa890630fbc73a99e1b713627f
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-08-20 20:55:10 -0400

    This is the point I got to before I realized I need to start over.

--- app.js
+++ app.js
@@ -2,43 +2,80 @@
 
 let interactiveContent = document.getElementById("interactiveWindow");
 
-function mainMenu(){
-  interactiveContent.innerHTML =
-  `<form id="form" name="form">
-  I want to play best of
-  <label for="bestOf"><input id="bestOf" name="bestOf" type="number" class="numberInput" value="3" min="3" step="2" max="23"></label> against 
-  <label for="againstWhom"><select id="againstWhom" name="againstWhom" class="dropdownInput">
-    <option value="computer">the computer</option>
-    <option value="human">the person next to me</option>
-  </select></label>.<br>
-  <div class="buttonHolder"><label for="start"><input id="startButton" type="submit" name="start" value="Start!"></label></div>
-</form>`;
-  let formData = document.getElementById("form");
-  form.onsubmit = function(e) {
-    e.preventDefault();
-    let bestOf = formData.bestOf.value;
-    let againstWhom = formData.againstWhom.value;
-    playGame(bestOf, againstWhom);
-  }
-} 
-
-function playGame(bestOf, againstWhom) {
-  if (againstWhom == "computer"){
-    playAgainstComputer(bestOf);
-  }
-  if (againstWhom == "human"){
-    playAgainstHuman(bestOf);
+class Player {
+  constructor(name){
+    this.name = name;
+    this.points = 0;
+    this.gestureArray = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    this.chosenGesture = null;
   }
 }
 
-function playAgainstComputer(bestOf){
-  let pointsToWin = bestOf / 2 + 0.5;
-  console.log(`You will now play against the computer. First to ${pointsToWin} points wins!`)
+class Human extends Player {
+  constructor(name){
+    super();
+  }
+  chooseGesture(){
+
+  }
 }
 
-function playAgainstHuman(bestOf){
-  let pointsToWin = bestOf / 2 + 0.5;
-  console.log(`You will now play against each other. First to ${pointsToWin} points wins!`)
+class Computer extends Player {
+  constructor(){
+    super();
+    }
+    chooseGesture(){
+
+    }
 }
+
+class Game{
+  mainMenu(){
+    interactiveContent.innerHTML =
+    `<form id="form" name="form">
+    I want to play best of
+    <label for="bestOf"><input id="bestOf" name="bestOf" type="number" class="numberInput" value="3" min="3" step="2" max="23"></label> against 
+    <label for="againstWhom"><select id="againstWhom" name="againstWhom" class="dropdownInput">
+      <option value="computer">the computer</option>
+      <option value="human">the person next to me</option>
+    </select></label>.<br>
+    <div class="buttonHolder"><label for="start"><input id="startButton" type="submit" name="start" value="Start!"></label></div>
+  </form>`;
+    let form = document.getElementById("form");
+    form.onsubmit = function(e) {
+      e.preventDefault();
+      let bestOf = form.bestOf.value;
+      let againstWhom = form.againstWhom.value;
+      this.playGame(bestOf, againstWhom);
+    }
+  } 
+  
+  playGame(bestOf, againstWhom) {
+    if (againstWhom == "computer"){
+      return playAgainstComputer(bestOf);
+    }
+    if (againstWhom == "human"){
+       return playAgainstHuman(bestOf);
+    }
+  }
+  
+  playAgainstComputer(bestOf){
+    let player = new Human;
+    let computer = new Computer;
+    let pointsToWin = bestOf / 2 + 0.5;
+    console.log(`You will now play against the computer. First to ${pointsToWin} points wins!`);
+  }
+  
+  playAgainstHuman(bestOf){
+    let player1 = new Human;
+    let player2 = new Human;
+    this.playerOne.chooseGesture();
+    this.playerTwo.chooseGesture();
+    let pointsToWin = bestOf / 2 + 0.5;
+    console.log(`You will now play against each other. First to ${pointsToWin} points wins!`);
+  }
+}
+
+let theGame = new Game();
 
 
