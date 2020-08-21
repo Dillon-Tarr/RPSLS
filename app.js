69e7685a789c274bc8607923796114e45a0ca143
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-08-20 21:43:41 -0400

    revised code so that I'm ready to start building the logic of the game

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 let interactiveContent = document.getElementById("interactiveWindow");
 
 class Player {
-  constructor(name){
+  constructor(name = "Player"){
     this.name = name;
     this.points = 0;
     this.gestureArray = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
@@ -30,32 +30,12 @@ class Computer extends Player {
 }
 
 class Game{
-  mainMenu(){
-    interactiveContent.innerHTML =
-    `<form id="form" name="form">
-    I want to play best of
-    <label for="bestOf"><input id="bestOf" name="bestOf" type="number" class="numberInput" value="3" min="3" step="2" max="23"></label> against 
-    <label for="againstWhom"><select id="againstWhom" name="againstWhom" class="dropdownInput">
-      <option value="computer">the computer</option>
-      <option value="human">the person next to me</option>
-    </select></label>.<br>
-    <div class="buttonHolder"><label for="start"><input id="startButton" type="submit" name="start" value="Start!"></label></div>
-  </form>`;
-    let form = document.getElementById("form");
-    form.onsubmit = function(e) {
-      e.preventDefault();
-      let bestOf = form.bestOf.value;
-      let againstWhom = form.againstWhom.value;
-      this.playGame(bestOf, againstWhom);
-    }
-  } 
-  
   playGame(bestOf, againstWhom) {
     if (againstWhom == "computer"){
-      return playAgainstComputer(bestOf);
+      return this.playAgainstComputer(bestOf);
     }
     if (againstWhom == "human"){
-       return playAgainstHuman(bestOf);
+       return this.playAgainstHuman(bestOf);
     }
   }
   
@@ -69,10 +49,8 @@ class Game{
   playAgainstHuman(bestOf){
     let player1 = new Human;
     let player2 = new Human;
-    this.playerOne.chooseGesture();
-    this.playerTwo.chooseGesture();
+
     let pointsToWin = bestOf / 2 + 0.5;
-    console.log(`You will now play against each other. First to ${pointsToWin} points wins!`);
   }
 }
 
